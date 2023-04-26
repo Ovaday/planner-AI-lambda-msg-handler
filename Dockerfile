d0d1d6cbdfd840a5242c8696888835c86343634a
Evgenii <evgeny.abdrazakov@truesalessoft.com>
2023-04-26 13:10:56 +0200

    Working audio transcribing

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,33 @@
 FROM public.ecr.aws/lambda/python:3.10
 
+#all files are stored in var/task
+RUN yum update -y && \
+    yum install -y gcc-c++ make wget tar cmake3 openssl-devel gzip stat \
+    flac-devel libogg-devel libvorbis-devel vorbis-tools alsa-lib-devel \
+    opus-devel lame-devel libmpg123-devel pkgconfig epel-release sqlite-devel libsamplerate sox libsndio ffmpeg
+
+RUN pip install --upgrade pip && \
+    pip install awscli
+
+RUN yum install -y xz which
+
+RUN curl -L -o ffmpeg.tar.xz https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-static.tar.xz && \
+    tar -xf ffmpeg.tar.xz --strip-components=1 -C /usr/bin/ && \
+    rm ffmpeg.tar.xz
+
+#RUN yum -y install ffmpeg ffmpeg-devel &&
+RUN which ffprobe && which ffmpeg
+
+ENV PATH="${PATH}:/usr/bin/cmake3"
+
+
 # Install the function's dependencies using file requirements.txt
 # from your project folder.
 
 COPY ./tg_routine/requirements.txt  .
-RUN yum -y install libsndfile
+#RUN yum -y install libsndfile
 RUN pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
-RUN python -m pip install --force-reinstall soundfile
-
-
-#all files are stored in var/task
-RUN yum update -y && yum install gcc-c++ make wget tar openssl-devel gzip stat -y
-RUN wget https://github.com/Kitware/CMake/releases/download/v3.24.0/cmake-3.24.0.tar.gz && \
-    tar -zxvf cmake-3.24.0.tar.gz && \
-    rm cmake-3.24.0.tar.gz
-RUN cd cmake-3.24.0 && ./bootstrap -- -DCMAKE_USE_OPENSSL=OFF && make && make install && cd ..
-RUN wget https://github.com/libsndfile/libsndfile/archive/refs/tags/1.1.0.tar.gz && \
-    tar -zxvf 1.1.0.tar.gz && \
-    rm 1.1.0.tar.gz && \
-    mv libsndfile-1.1.0 ${LAMBDA_TASK_ROOT}/libsndfile
-RUN cd libsndfile && cmake -G"Unix Makefiles" && make && make install && cd ..
+#RUN python -m pip install --force-reinstall soundfile
 
 # Copy function code
 #COPY ./tg_routine/* ${LAMBDA_TASK_ROOT}/
--- tg_routine/audioHandlers.py
+++ tg_routine/audioHandlers.py
@@ -6,7 +6,7 @@ from serviceHelpers import check_is_chat_approved
 
 import io
 import json
-import soundfile as sf
+#import soundfile as sf
 
 
 async def audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -25,9 +25,9 @@ async def audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print('recognized_text')
         #await async_assign_last_conversation(chat_id, recognized_text)
         await context.bot.send_message(reply_to_message_id=message.message_id, chat_id=chat_id,
-                                       text=f"""{get_label('recognized', chat.language)}: {recognized_text}
+                                       text=f"""Recognized text: {recognized_text}
 
-        {get_label('processing_wait', chat.language)}""")
+        Processing, please wait...""")
 
         #json_update = json.loads(update.to_json())
         #json_update = audio_json_to_text(json_update, recognized_text)
@@ -37,27 +37,24 @@ async def audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def process_voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    from pydub import AudioSegment
     voice_message = await context.bot.get_file(update.message.voice.file_id)
-    #voice_file = io.BytesIO()
-    #await voice_message.download_to_memory(voice_file)
     local_path = "/tmp/voice_message.ogg"
-    voice_message.download(local_path)
-    s3_client = boto3.client("s3")
+    await voice_message.download_to_drive(local_path)
 
-    s3_bucket = os.environ["VOICE_MESSAGES_BUCKET"]
-    s3_prefix = f'voice_{update.message.chat_id}_{update.message.message_id}.ogg'
-    remote_s3_path = os.path.join("s3://", s3_bucket, s3_prefix)
-    print(remote_s3_path)
-    s3_client.upload_file(local_path, s3_bucket, s3_prefix)
+    ogg_audio = AudioSegment.from_file(local_path, format="ogg")
 
-    #voice_file.seek(0)
-    #voice_file.name = f'voice_{update.message.chat_id}_{update.message.message_id}.ogg'
-
-    data, samplerate = sf.read(local_path)
+    # Convert the audio file to wav format
     mem_file = io.BytesIO()
-    sf.write(mem_file, data, samplerate, 'PCM_16', format='wav')
-    mem_file.seek(0)
-    duration = sf.info(mem_file).duration
+    ogg_audio.export(mem_file, format="wav")
+    #wav_audio.export(mem_file, format="wav")
+
+
+    #data, samplerate = sf.read(local_path)
+    #mem_file = io.BytesIO()
+    #sf.write(mem_file, data, samplerate, 'PCM_16', format='wav')
+    #mem_file.seek(0)
+    duration = ogg_audio.duration_seconds
     mem_file.seek(0)
     mem_file.name = f'voice_{update.message.chat_id}_{update.message.message_id}.wav'
     return mem_file, duration
